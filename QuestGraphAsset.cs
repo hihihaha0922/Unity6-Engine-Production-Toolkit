db1acf2d392bb01e83c8c5f63b42548da548693e
hihihaha0922 <jackee0922@gmail.com>
2026-06-12 16:44:47 +0800

    Delete QuestGraphAsset.cs

--- QuestGraphAsset.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewQuestGraph", menuName = "Larian Tools/Quest Graph Asset")]
-public class QuestGraphAsset : ScriptableObject
-{
-    // Holds every node belonging to this specific dialogue tree
-    public List<QuestNodeData> allNodes = new List<QuestNodeData>();
-}
